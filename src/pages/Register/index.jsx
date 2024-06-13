a4d22f7d5d00b7a42d874f56fad88d17865d06ff
Jimmiejay <phattharasuda27417@gmail.com>
2024-06-13 10:32:22 +0700

    update page/register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Header from '../../components/Header'
 import Register from '../../components/Register'
 import Footer from '../../components/Footer'
-// import Main from '../../components/Main'
-import RegisterButton from '../../components/RegisterButton'
 import './style.css'
 
 function RegisterPage() {
