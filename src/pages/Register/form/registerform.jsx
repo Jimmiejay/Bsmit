30ce7edfa26a996128915ad253750ab313beea5f
Jimmiejay <phattharasuda27417@gmail.com>
2024-06-13 12:53:51 +0700

    update register form

--- src/pages/Register/form/registerform.jsx
+++ src/pages/Register/form/registerform.jsx
@@ -6,7 +6,7 @@ const registerform = () => {
     <div className='registerform'>
       <div className='div'>
         <label className='label'>อีเมล</label>
-        <p>สำหรับการเข้าสู่ระบบและรับการติดต่อจากนายจ้าง</p>
+        <h3>สำหรับการเข้าสู่ระบบและรับการติดต่อจากนายจ้าง</h3>
         <input className='input' type="email" id="email" name="email" required  placeholder="กรอกอีเมล" />
       </div>
       <div className='div'>
