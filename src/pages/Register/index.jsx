55b477c223345e167c22aebf10727701b895a793
Jimmiejay <phattharasuda27417@gmail.com>
2024-06-13 10:31:22 +0700

    update page/register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,36 +1,22 @@
 import React from 'react'
 import Header from '../../components/Header'
-<<<<<<< HEAD
 import Register from '../../components/Register'
-=======
 import Footer from '../../components/Footer'
 // import Main from '../../components/Main'
 import RegisterButton from '../../components/RegisterButton'
->>>>>>> adb661f861458fc9509a65e43a758793713a71fc
 import './style.css'
 
 function RegisterPage() {
   return (
-<<<<<<< HEAD
+    <>
       <div className='register-container'>
         <Header />
         <main>
           <Register />
         </main>
       </div>
-=======
-    <>
-      <div className='register-container'>
-        <Header />
-        <main>
-          Register Page
-          <RegisterButton />
-          {/* <Main /> */}
-        </main>
-      </div>
       <Footer />
     </>
->>>>>>> adb661f861458fc9509a65e43a758793713a71fc
   )
 }
 
