5f9c69fd7b5b99b050bbd4cbf0cbdfa2c12b7b92
Jimmiejay <phattharasuda27417@gmail.com>
2024-06-14 09:53:20 +0700

    update icon

--- src/pages/Register/form/registerform.jsx
+++ src/pages/Register/form/registerform.jsx
@@ -3,28 +3,29 @@ import './style.css'
 import { FaEyeSlash, FaEye } from 'react-icons/fa';
 
 const registerform = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    const togglePasswordVisibility = () => {
-      setShowPassword(!showPassword);
-    };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const toggleConfirmPasswordVisibility = () => {
-      setShowConfirmPassword(!showConfirmPassword);
-    };
-    return (
-      <div className='registerform'>
-        <div>
-          <h1>สมัครสมาชิก</h1>
-          <label className='label'>
-            <h2>อีเมล</h2>
-            <h3>สำหรับการเข้าสู่ระบบและรับการติดต่อจากนายจ้าง</h3>
-          </label>
-          <input className='input' type="email" id="email" name="email" required placeholder="กรอกอีเมล" />
-        </div>
-        <div>
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+  return (
+    <div className='registerform'>
+      <div>
+        <h1>สมัครสมาชิก</h1>
+        <label className='label'>
+          <h2>อีเมล</h2>
+          <h3>สำหรับการเข้าสู่ระบบและรับการติดต่อจากนายจ้าง</h3>
+        </label>
+        <input className='input' type="email" id="email" name="email" required placeholder="กรอกอีเมล" />
+      </div>
+      <div>
         <label className='label'><h2>รหัสผ่าน</h2></label>
+        <div className='input-group'>
           <input
             className='input' type={showPassword ? 'text' : 'password'} id="password" name="password" required
             placeholder="กรอกรหัสผ่าน" />
@@ -32,39 +33,42 @@ const registerform = () => {
             {showPassword ? <FaEyeSlash /> : <FaEye />}
           </span>
         </div>
-        <div>
+      </div>
+      <div>
+        <div className='input-group'>
           <label className='label'><h2>ยืนยันรหัสผ่าน</h2></label>
-          <input className='input' type={showConfirmPassword ? 'text' : 'password'} id="confirmPassword" name="confirmPassword" required 
-          placeholder="กรอกรหัสผ่าน" />
+          <input className='input' type={showConfirmPassword ? 'text' : 'password'} id="confirmPassword" name="confirmPassword" required
+            placeholder="กรอกรหัสผ่าน" />
           <span className="toggle-icon2" onClick={toggleConfirmPasswordVisibility}>
             {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
           </span>
         </div>
-        <div className="terms-container">
-          <div className="checkbox-container">
-            <input type="checkbox" id="termsAndConditions" name="termsAndConditions" required />
-            <div className='terms-text'>
-              <h2>
-                ฉันยอมรับ นโนบายความเป็นส่วนตัว และยินยอมให้เก็บ
-                รวบรวม และใช้ข้อมูลส่วนบุคคลของฉันตามที่ระบุ
-                ในนโยบายดังกล่าว ฉันตกลงที่จะรับข้อความ
-                ทางการตลาดจาก BSMIT และบริษัทในเครือ
-                และเข้าใจว่าฉันสามารถเพิกถอน
-                ความยินยอมเมื่อใดก็ได้ผ่านทางลิงก์ยกเลิกการรับข้อความ
-                หรือตามรายละเอียดในนโนบายความเป็นส่วนตัว
-              </h2>
-            </div>
+      </div>
+      <div className="terms-container">
+        <div className="checkbox-container">
+          <input type="checkbox" id="termsAndConditions" name="termsAndConditions" required />
+          <div className='terms-text'>
+            <h2>
+              ฉันยอมรับ นโนบายความเป็นส่วนตัว และยินยอมให้เก็บ
+              รวบรวม และใช้ข้อมูลส่วนบุคคลของฉันตามที่ระบุ
+              ในนโยบายดังกล่าว ฉันตกลงที่จะรับข้อความ
+              ทางการตลาดจาก BSMIT และบริษัทในเครือ
+              และเข้าใจว่าฉันสามารถเพิกถอน
+              ความยินยอมเมื่อใดก็ได้ผ่านทางลิงก์ยกเลิกการรับข้อความ
+              หรือตามรายละเอียดในนโนบายความเป็นส่วนตัว
+            </h2>
           </div>
         </div>
-        <div>
-          <button className='button' type="sumit">ลงทะเบียน</button>
-        </div>
-        <div className='nouser-text'>
-          <h2 style={{ display: 'inline-block' }}>คุณมีบัญชีผู้ใช้อยู่แล้วหรือไม่?</h2>
-          <h4 style={{ display: 'inline-block' }}>เข้าสู่ระบบ</h4>
-        </div>
       </div>
-    )
-  }
+      <div>
+        <button className='button' type="sumit">ลงทะเบียน</button>
+      </div>
+      <div className='nouser-text'>
+        <h2 style={{ display: 'inline-block' }}>คุณมีบัญชีผู้ใช้อยู่แล้วหรือไม่?</h2>
+        <h4 style={{ display: 'inline-block' }}>เข้าสู่ระบบ</h4>
+      </div>
+    </div>
+  )
+}
 
-  export default registerform
+export default registerform
